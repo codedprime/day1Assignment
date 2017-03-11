4ea6a08a26f5bed4fb9c7e30f73c80f3a46bc939
codedprime <socially4fun@gmail.com>
2017-03-11 12:18:34 +0100

    Added fizzBuzz in app

---
+++ app/fizzBuzz.js
@@ -0,0 +1,32 @@
+function fizzBuzz(A) {
+
+   
+        var answer = '';
+    if ((typeof A =='string') || (A===0))  {
+    return ('invalid');
+        }
+
+        if (A%3 === 0 ){
+        
+           answer=  'Fizz';
+        }
+        if(A%5 === 0 ){
+        
+            answer += 'Buzz'          
+         }    
+        
+         if  ((A%3 !==0) && (A%5 !==0)){            
+       return (A)
+        } 
+          else if  ((A%3 ===0) || (A%5 ===0)){            
+       return (answer)
+        }  
+
+        
+        else
+        {
+           return (A)
+        }   
+    }        
+
+
