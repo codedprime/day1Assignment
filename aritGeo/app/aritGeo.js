c7faa584596b03ef96971fb23280c4c6d5c40c5d
Obinna Okwuolisa <codedprime@users.noreply.github.com>
2017-03-18 09:16:29 +0100

    Made some changes to the app and added package

---
+++ aritGeo/app/aritGeo.js
@@ -0,0 +1,20 @@
+module.exports = {
+	aritGeo: function(A){	  
+		var i = A.length;	
+		if ((typeof A !== 'object') || (typeof A[0] =="string") ) {	    
+			return ('invalid');	        
+		}	
+
+		if (i===0){	  
+			return 0;	
+		}	 
+		else if ((A[i-1] - A[i-2]) == (A[i-2] - A[i-3])){	   
+			return 'Arithmetic'; 
+		}	 
+		else if (Math.abs((A[i-1] / A[i-2])) == Math.abs((A[i-2] / A[i-3]))){	   
+			return 'Geometric';
+		}else{	   
+			return -1	 
+		}
+	}
+}
